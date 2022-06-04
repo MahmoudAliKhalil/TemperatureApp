a5f2e475b51d7ad95d6f7cfc20b898107063be8f
Mahmoud Ali Khalil <mahmoud.alikhalil@gmail.com>
2022-06-04 23:09:35 +0200

    Remove comment

--- temperature-api/src/main/java/io/github/mahmoudalikhalil/temperatureapi/repository/TemperatureRepository.java
+++ temperature-api/src/main/java/io/github/mahmoudalikhalil/temperatureapi/repository/TemperatureRepository.java
@@ -14,7 +14,6 @@ public interface TemperatureRepository extends PagingAndSortingRepository<Temper
     @Query("select temp from Temperature temp where temp.id.deviceId = :deviceId order by temp.id.timestamp desc")
     List<Temperature> findDeviceTemperatures(@Param("deviceId") Long deviceId);
 
-//    @Query(value = "SELECT t1.timestamp, t1.device_id, t1.temperature FROM temperature AS t1 INNER JOIN (SELECT MAX(timestamp) AS leastTime, device_id FROM temperature GROUP BY device_id) AS t2 ON t1.timestamp = t2.leastTime AND t1.device_id = t2.device_id ORDER BY t1.timestamp DESC", nativeQuery = true)
     @Query("SELECT t1 FROM Temperature t1 where t1.id.timestamp = (SELECT MAX(t2.id.timestamp) FROM Temperature t2 where t1.id.deviceId = t2.id.deviceId) ORDER BY t1.id.timestamp")
     List<Temperature> getRecentTemperatures();
 }
